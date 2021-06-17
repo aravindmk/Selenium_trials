9d6e32c8b2e89738355d33da394643d1aa8723bf
d4rk3n <aravindmk@outlook.com>
2021-06-17 08:32:01 +0530

    .

--- Video_selector.py
+++ Video_selector.py
@@ -7,11 +7,9 @@ browser.get('https://www.youtube.com/results?search_query=sreeragamo')
 
 print("You are accessing :",browser.title)
 
+#time.sleep(10)
+#title = browser.find_elements_by_id("video-title")
+#print(len(title))
 
-thumbnail_element = browser.find_elements_by_id("thumbnail")
-first = thumbnail_element[0]
-first.send_keys(Keys.RETURN)
-
-views = browser.find_elements_by_name(" views")
-print(views)
-browser.quit()
+filters = browser.find_element_by_css_selector("Filters")
+filters.send_keys(Keys.RETURN)
